9ece675c17a1177c9ead0fadb62049c3d3cb3a7c
appwebtech <josembi@gmail.com>
2016-05-08 10:23:40 +0200

    Added pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'add_pages/home'
+
+  get 'add_pages/help'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
